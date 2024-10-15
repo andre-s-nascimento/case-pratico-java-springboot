be4058416b6a4e83f54166c9a7ed6c865de43ffd
André S Nascimento <Andre.Nascimento.Contractor@pepsico.com>
2024-10-15 20:06:15 -0300

    Inserindo mais dados no data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,3 @@
 INSERT INTO produto (nome, preco, descricao, quantidade_em_estoque) VALUES ('Produto A', 10.0, 'Descrição do Produto A', 100);
 INSERT INTO produto (nome, preco, descricao, quantidade_em_estoque) VALUES ('Produto B', 15.5, 'Descrição do Produto B', 200);
+INSERT INTO produto (nome, preco, descricao, quantidade_em_estoque) VALUES ('Produto C', 30.2, 'Descrição do Produto C', 300);
